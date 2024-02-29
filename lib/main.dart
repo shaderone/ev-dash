7bd59f9de81317320c1fb139ed4399b5e5c85f52
pixiedust07 <sharonshaji127001@gmail.com>
2024-03-01 00:21:54 +0530

    Add splash with transition

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:car_dashbord/splash_screen.dart/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/splash_screen.dart/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:car_dashbord/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (context) {
+            _controller.forward();
+            return FadeTransition(
+              opacity: _animation,
+              child: const Dashboard(),
+            );
+          },
+        ));
+      },
+    );
+
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 5));
+
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+
+    @override
+    void dispose() {
+      _controller.dispose();
+      super.dispose();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const SizedBox.expand(
+        child: Image(
+          image: AssetImage("assets/icons/bg.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ).animate().fade(duration: const Duration(seconds: 5)).fadeOut(),
+    );
+  }
+}
